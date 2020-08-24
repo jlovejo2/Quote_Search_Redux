9a0401ca1ca02158352486b21d5d059378e46c90
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-24 17:00:21 -0500

    added api info to its own file as an array of json objects

---
+++ src/api/apiInfo.js
@@ -0,0 +1,21 @@
+const quotesApiArray = [
+  {
+    buttonHeader: "Needing some Inspiration from the Great Ron Swanson?",
+    buttonText: "click here",
+    cardHeader: "Ron Freaking Swanson!!",
+  },
+  {
+    buttonHeader: "Can't think of what kind of quote you want?",
+    buttonText: "Try somethind random",
+    cardHeader: "Random Quotes",
+  },
+  {
+    buttonHeader: "Looking for quotes from a movie?",
+    buttonText: "Let her rip!",
+    cardHeader: "Movie Quotes",
+  },
+];
+
+module.exports = {
+  quotesApiArray,
+};
