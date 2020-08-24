fe2a639e8d7fca341f802d674bf3842ee4177e9d
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-24 10:39:50 -0500

    forgot return in jumbotron

--- src/components/common/Jumbotron.js
+++ src/components/common/Jumbotron.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 const Jumbotron = ({ headerOne, descriptionOne, buttonText, children }) => {
-  <div className="jumbotron">
-    <div className="row">
-      <h1>{headerOne}</h1>
-      <p>{descriptionOne}</p>
-      <hr className="my-4"></hr>
+  return (
+    <div className="jumbotron">
+      <div className="row">
+        <h1>{headerOne}</h1>
+        <p>{descriptionOne}</p>
+        <hr className="my-4"></hr>
+      </div>
+      <div className="row">
+        <p>{buttonText ? buttonText : ""}</p>
+      </div>
+      {children}
     </div>
-    <div className="row">
-      <p>{buttonText ? buttonText : ""}</p>
-    </div>
-    {children}
-  </div>;
+  );
 };
 
 export default Jumbotron;
