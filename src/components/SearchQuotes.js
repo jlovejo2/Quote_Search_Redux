091d5fac5ff6971f841797726c1e924b36b44874
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-24 11:18:53 -0500

    added spinner for search quote page when swanson quote is loading

--- src/components/SearchQuotes.js
+++ src/components/SearchQuotes.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { loadRonSwansonQuotes } from "../redux/actions/ronSwansonApiActions";
 import { connect } from "react-redux";
 import Jumbotron from "../components/common/Jumbotron";
+import Spinner from "../components/common/Spinner";
 
 export function SearchQuotes({
   quotes,
   loadRonSwansonQuotes,
   ronSwanson,
+  loading,
   history,
   ...props
 }) {
@@ -35,6 +37,26 @@ export function SearchQuotes({
     // setRonSwansonQuotes({ ...ronSwansonQuotes, one: quotes });
   };
 
+  const ronSwansonQuoteFragment = loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <ul className={"list-group"}>
+        {ronSwanson.length > 0 ? (
+          ronSwanson.map((value, index) => {
+            return (
+              <li className={"list-item p-2"} key={index}>
+                {value}
+              </li>
+            );
+          })
+        ) : (
+          <li> No quotes yet. </li>
+        )}
+      </ul>
+    </Fragment>
+  );
+
   return (
     <>
       <Jumbotron
@@ -49,21 +71,7 @@ export function SearchQuotes({
         </button>
       </Jumbotron>
       <div className="container">
-        <div className="row">
-          <ul className={"list-group"}>
-            {ronSwanson.length > 0 ? (
-              ronSwanson.map((value, index) => {
-                return (
-                  <li className={"list-item p-2"} key={index}>
-                    {value}
-                  </li>
-                );
-              })
-            ) : (
-              <li> No quotes yet. </li>
-            )}
-          </ul>
-        </div>
+        <div className="row">{ronSwansonQuoteFragment}</div>
       </div>
     </>
   );
@@ -74,6 +82,7 @@ SearchQuotes.propTypes = {
   ronSwanson: PropTypes.array.isRequired,
   history: PropTypes.object.isRequired,
   loadRonSwansonQuotes: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 function mapStateToProps(state, ownProps) {
@@ -83,6 +92,7 @@ function mapStateToProps(state, ownProps) {
   return {
     ronSwanson: state.ronSwanson.length === 0 ? [] : state.ronSwanson,
     quotes: [],
+    loading: state.apiCallsInProgress > 0,
   };
 }
 
