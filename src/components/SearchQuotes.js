674ce0b4a65d9d184a0e3b6b42a97712c61b7034
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-25 11:21:59 -0500

    added code for kanyeWest functionality into searchQuotes page

--- src/components/SearchQuotes.js
+++ src/components/SearchQuotes.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { loadRonSwansonQuotes } from "../redux/actions/ronSwansonApiActions";
 import { loadRandQuoteGardenQuote } from "../redux/actions/quoteGardenApiActions";
+import { loadKanyeWestQuotes } from "../redux/actions/kanyeWestApiActions";
 import { connect } from "react-redux";
 import Jumbotron from "../components/common/Jumbotron";
 import Spinner from "../components/common/Spinner";
@@ -14,8 +15,10 @@ export function SearchQuotes({
   quotes,
   loadRonSwansonQuotes,
   loadRandQuoteGardenQuote,
+  loadKanyeWestQuotes,
   ronSwanson,
   quoteGarden,
+  kanyeWest,
   loading,
   history,
   ...props
@@ -36,6 +39,8 @@ export function SearchQuotes({
         await loadRandQuoteGardenQuote();
       } else if (apiIndex === quotesApiArray[2].name) {
         console.log(quotesApiArray[2].name);
+      } else if (apiIndex === quotesApiArray[3].name) {
+        await loadKanyeWestQuotes();
       }
       // setRonSwansonQuotes(...ronSwansonQuotes);
       console.log(quotes);
@@ -131,9 +136,11 @@ SearchQuotes.propTypes = {
   quotes: PropTypes.array.isRequired,
   ronSwanson: PropTypes.array.isRequired,
   quoteGarden: PropTypes.array.isRequired,
+  kanyeWest: PropTypes.array.isRequired,
   history: PropTypes.object.isRequired,
   loadRonSwansonQuotes: PropTypes.func.isRequired,
   loadRandQuoteGardenQuote: PropTypes.func.isRequired,
+  loadKanyeWestQuotes: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
 };
 
@@ -144,6 +151,7 @@ function mapStateToProps(state, ownProps) {
   return {
     ronSwanson: state.ronSwanson.length === 0 ? [] : state.ronSwanson,
     quoteGarden: state.quoteGarden.length === 0 ? [] : state.quoteGarden,
+    kanyeWest: state.kanyeWest.length === 0 ? [] : state.kanyeWest,
     quotes: [],
     loading: state.apiCallsInProgress > 0,
   };
@@ -152,6 +160,7 @@ function mapStateToProps(state, ownProps) {
 const mapDispatchToProps = {
   loadRonSwansonQuotes,
   loadRandQuoteGardenQuote,
+  loadKanyeWestQuotes,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchQuotes);
