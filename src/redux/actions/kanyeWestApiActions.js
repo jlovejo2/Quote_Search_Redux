6d34799fdf1c2723eff001edb2c44dc352ec5c3b
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-25 11:16:49 -0500

    added kanye west quote success to action types, added code for thunk to get quote from api

---
+++ src/redux/actions/kanyeWestApiActions.js
@@ -0,0 +1,31 @@
+import * as types from "./actionTypes";
+import * as kanyeWestApi from "../../api/kanyeWestApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadKanyeWestQuotesSuccess(quotes) {
+  return {
+    type: types.LOAD_KANYE_WEST_QUOTES_SUCCESS,
+    kanyeWest: quotes,
+  };
+}
+
+//thunk
+export function loadRonSwansonQuotes() {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+    return kanyeWestApi
+      .getKanyeWest()
+      .then((resp) => {
+        const author = "The Kanye West";
+        const quoteObj = {
+          quote: resp.quote,
+          author: author,
+        };
+        dispatch(loadKanyeWestQuotesSuccess(quoteObj));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
