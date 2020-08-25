a604389e11a75702f6296fdae490ffed9b35baa7
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-24 19:47:20 -0500

    added names to apiInfo object

--- src/api/apiInfo.js
+++ src/api/apiInfo.js
@@ -1,15 +1,18 @@
 const quotesApiArray = [
   {
+    name: "Ron Swanson Api",
     buttonHeader: "Needing some Inspiration from the Great Ron Swanson?",
     buttonText: "click here",
     cardHeader: "Ron Freaking Swanson!!",
   },
   {
+    name: "Random Quote Api",
     buttonHeader: "Can't think of what kind of quote you want?",
     buttonText: "Try somethind random",
     cardHeader: "Random Quotes",
   },
   {
+    name: "Movie Quote Api",
     buttonHeader: "Looking for quotes from a movie?",
     buttonText: "Let her rip!",
     cardHeader: "Movie Quotes",
