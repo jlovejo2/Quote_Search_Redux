8eec248e3b873fbaa27ea94b5d3de8db5e0b905d
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-26 12:05:25 -0500

    renamed quoteUL to QuoteCard no longer using unorder list

---
+++ src/components/common/QuoteCard.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-regular-svg-icons";
+
+const quoteUnorderList = ({ quoteArray }) => {
+  return (
+    <Fragment>
+      {quoteArray.length > 0
+        ? quoteArray.map((value, index) => {
+            return (
+              <Card key={index}>
+                <Card.Img
+                  variant="top"
+                  src="#"
+                  alt={`${value.author}`}
+                ></Card.Img>
+                <Card.Body>
+                  <p>{`"${value.quote}"`}</p>
+                  <footer>
+                    <div className="row justify-space-between">
+                      <div className="col">{`-${value.author}`}</div>
+                      <div className="col text-center">
+                        <button className="quote-UL_icon-button">
+                          <FontAwesomeIcon icon={faThumbsUp} />
+                        </button>
+                        <button className="quote-UL_icon-button">
+                          <FontAwesomeIcon icon={faThumbsDown} />
+                        </button>
+                      </div>
+                    </div>
+                  </footer>
+                </Card.Body>
+              </Card>
+            );
+          })
+        : null}
+    </Fragment>
+  );
+};
+
+quoteUnorderList.propTypes = {
+  quoteArray: PropTypes.array.isRequired,
+};
+
+export default quoteUnorderList;
