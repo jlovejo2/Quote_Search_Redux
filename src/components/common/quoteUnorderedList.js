0f8b3fefa444b176db28b1f533777c583541c6e4
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-26 08:55:59 -0500

    added cards to rendered quotes display

--- src/components/common/quoteUnorderedList.js
+++ src/components/common/quoteUnorderedList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Card } from "react-bootstrap";
 
 const quoteUnorderList = ({ quoteArray }) => {
   return (
@@ -8,7 +9,13 @@ const quoteUnorderList = ({ quoteArray }) => {
         quoteArray.map((value, index) => {
           return (
             <li className={"list-item p-2"} key={index}>
-              {`"${value.quote}"  -${value.author}`}
+              <Card>
+                <Card.Header>header</Card.Header>
+                <Card.Body>
+                  <p>{`"${value.quote}"`}</p>
+                  <footer>{`-${value.author}`}</footer>
+                </Card.Body>
+              </Card>
             </li>
           );
         })
