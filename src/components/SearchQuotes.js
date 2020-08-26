c7bfec61794448279e9e7891e20bb1556cfbfcd8
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-26 09:23:12 -0500

    import taylor swift load action and taylorSwift array from initial state to search quotes.js.  Added propType definitions

--- src/components/SearchQuotes.js
+++ src/components/SearchQuotes.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { loadRonSwansonQuotes } from "../redux/actions/ronSwansonApiActions";
 import { loadRandQuoteGardenQuote } from "../redux/actions/quoteGardenApiActions";
 import { loadKanyeWestQuotes } from "../redux/actions/kanyeWestApiActions";
+import { loadTaylorSwiftQuotes } from "../redux/actions/taylorSwiftApiActions";
 import { connect } from "react-redux";
 import Jumbotron from "../components/common/Jumbotron";
 import Spinner from "../components/common/Spinner";
@@ -16,9 +17,11 @@ export function SearchQuotes({
   loadRonSwansonQuotes,
   loadRandQuoteGardenQuote,
   loadKanyeWestQuotes,
+  loadTaylorSwiftQuotes,
   ronSwanson,
   quoteGarden,
   kanyeWest,
+  taylorSwift,
   loading,
   history,
   ...props
@@ -41,11 +44,14 @@ export function SearchQuotes({
         console.log(quotesApiArray[2].name);
       } else if (apiIndex === quotesApiArray[3].name) {
         await loadKanyeWestQuotes();
+      } else if (apiIndex === quotesApiArray[4].name) {
+        await loadTaylorSwiftQuotes();
       }
       // setRonSwansonQuotes(...ronSwansonQuotes);
       console.log(quotes);
       console.log(ronSwanson);
       console.log(quoteGarden);
+      console.log(taylorSwift);
     } catch (error) {
       const newError = {};
       newError["ronSwansonApiError"] = setErrors({ ...errors, newError });
@@ -144,10 +150,12 @@ SearchQuotes.propTypes = {
   ronSwanson: PropTypes.array.isRequired,
   quoteGarden: PropTypes.array.isRequired,
   kanyeWest: PropTypes.array.isRequired,
+  taylorSwift: PropTypes.array.isRequired,
   history: PropTypes.object.isRequired,
   loadRonSwansonQuotes: PropTypes.func.isRequired,
   loadRandQuoteGardenQuote: PropTypes.func.isRequired,
   loadKanyeWestQuotes: PropTypes.func.isRequired,
+  loadTaylorSwiftQuotes: PropTypes.func.isRequired,
   loading: PropTypes.bool.isRequired,
 };
 
@@ -159,6 +167,7 @@ function mapStateToProps(state, ownProps) {
     ronSwanson: state.ronSwanson.length === 0 ? [] : state.ronSwanson,
     quoteGarden: state.quoteGarden.length === 0 ? [] : state.quoteGarden,
     kanyeWest: state.kanyeWest.length === 0 ? [] : state.kanyeWest,
+    taylorSwift: state.taylorSwift.length === 0 ? [] : state.taylorSwift,
     quotes: [],
     loading: state.apiCallsInProgress > 0,
   };
@@ -168,6 +177,7 @@ const mapDispatchToProps = {
   loadRonSwansonQuotes,
   loadRandQuoteGardenQuote,
   loadKanyeWestQuotes,
+  loadTaylorSwiftQuotes,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchQuotes);
