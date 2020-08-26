4684b864a2700cb1a736b98456ef7c6798a5bf83
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-26 11:54:55 -0500

    fixed multiple rendering of "no quotes yet"

--- src/components/common/quoteUnorderedList.js
+++ src/components/common/quoteUnorderedList.js
@@ -7,39 +7,37 @@ import { faThumbsUp, faThumbsDown } from "@fortawesome/free-regular-svg-icons";
 const quoteUnorderList = ({ quoteArray }) => {
   return (
     <ul className={"list-group"}>
-      {quoteArray.length > 0 ? (
-        quoteArray.map((value, index) => {
-          return (
-            <li className={"list-item p-2"} key={index}>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src="#"
-                  alt={`${value.author}`}
-                ></Card.Img>
-                <Card.Body>
-                  <p>{`"${value.quote}"`}</p>
-                  <footer>
-                    <div className="row justify-space-between">
-                      <div className="col">{`-${value.author}`}</div>
-                      <div className="col text-center">
-                        <button className="quote-UL_icon-button">
-                          <FontAwesomeIcon icon={faThumbsUp} />
-                        </button>
-                        <button className="quote-UL_icon-button">
-                          <FontAwesomeIcon icon={faThumbsDown} />
-                        </button>
+      {quoteArray.length > 0
+        ? quoteArray.map((value, index) => {
+            return (
+              <li className={"list-item p-2"} key={index}>
+                <Card>
+                  <Card.Img
+                    variant="top"
+                    src="#"
+                    alt={`${value.author}`}
+                  ></Card.Img>
+                  <Card.Body>
+                    <p>{`"${value.quote}"`}</p>
+                    <footer>
+                      <div className="row justify-space-between">
+                        <div className="col">{`-${value.author}`}</div>
+                        <div className="col text-center">
+                          <button className="quote-UL_icon-button">
+                            <FontAwesomeIcon icon={faThumbsUp} />
+                          </button>
+                          <button className="quote-UL_icon-button">
+                            <FontAwesomeIcon icon={faThumbsDown} />
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </footer>
-                </Card.Body>
-              </Card>
-            </li>
-          );
-        })
-      ) : (
-        <li> No quotes yet. </li>
-      )}
+                    </footer>
+                  </Card.Body>
+                </Card>
+              </li>
+            );
+          })
+        : null}
     </ul>
   );
 };
