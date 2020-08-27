16db4e4dcfc6353c33b104659ec0ad5c7d580f00
James Lovejoy <james.lovejoy2@gmail.com>
2020-08-27 18:51:17 -0500

    added image for ron swans api to apiInfo and found relative path to get image to render to page

--- src/api/apiInfo.js
+++ src/api/apiInfo.js
@@ -1,30 +1,35 @@
 const quotesApiArray = [
   {
     name: "Ron Swanson Api",
+    imgLink: "src/assets/images/ron-swanson.jpg",
     buttonHeader: "Needing some Inspiration from the Great Ron Swanson?",
     buttonText: "click here",
     cardHeader: "Ron Freaking Swanson!!",
   },
   {
     name: "Quote Garden Api",
+    imgLink: "",
     buttonHeader: "Can't think of what kind of quote you want?",
     buttonText: "Try something random",
     cardHeader: "Random Quotes",
   },
   {
     name: "Kanye West Api",
+    imgLink: "",
     buttonHeader: "Wisdom from Kanye",
     buttonText: "Click if you think you can handle it",
     cardHeader: "The Kanye West",
   },
   {
     name: "Taylor Swift Api",
+    imgLink: "",
     buttonHeader: "Be inspired by Taylor Swift",
     buttonText: "Click here",
     cardHeader: "Taylor Swift",
   },
   {
     name: "Donald Trump Api",
+    imgLink: "",
     buttonHeader: "Less quotes and more stuff he's said...",
     buttonText: "Good luck",
     cardHeader: "Trump at his best",
