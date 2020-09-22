dbf5dd7b38ef4be378de3a2c92d755d1c43862f6
James Lovejoy <james.lovejoy2@gmail.com>
2020-09-22 14:17:04 -0500

    added quoteApi file with getQuote function

---
+++ src/api/quoteApi.js
@@ -0,0 +1,28 @@
+import { handleResponse, handleError } from "./apiUtils";
+const baseUrl = process.env.API_URL + "/quotes/";
+
+export function getQuotes() {
+  //fetch defaults to get
+  console.log(baseUrl);
+  return fetch(baseUrl).then(handleResponse).catch(handleError);
+}
+
+// export function saveQuote(quote) {
+//   console.log("base url", baseUrl);
+//   console.log("course", quote);
+//   console.log("course id", quote.id);
+
+//   return fetch(baseUrl + (quote.id || ""), {
+//     method: quote.id ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+//     headers: { "content-type": "application/json" },
+//     body: JSON.stringify(quote),
+//   })
+//     .then(handleResponse)
+//     .catch(handleError);
+// }
+
+// export function deleteCourse(quoteId) {
+//   return fetch(baseUrl + quoteId, { method: "DELETE" })
+//     .then(handleResponse)
+//     .catch(handleError);
+// }
