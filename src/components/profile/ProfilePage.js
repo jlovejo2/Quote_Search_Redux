2472d187220798e5a592df0675a09673af52198e
James Lovejoy <james.lovejoy2@gmail.com>
2020-09-22 15:12:14 -0500

    updated ProfilePage

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { loadLikedQuotes } from "../../redux/actions/likedQuotesActions";
+import Spinner from "../common/Spinner";
+import { toast } from "react-toastify";
 
 const ProfilePage = () => (
   <div className="jumbotron">
@@ -11,7 +16,40 @@ const ProfilePage = () => (
   </div>
 );
 
-export default ProfilePage;
+ProfilePage.propTypes = {
+  likedQuotes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+};
+
+//this type of function is commonly called a selector function
+function getCourseBySlug(courses, slug) {
+  return courses.find((course) => course.slug === slug || null);
+}
+
+//redux mapping function that indicates what states we'd like access to
+//mapStateToProps will run each time the redux store state changes
+//ownProps is automatically populated by react, it gives us access to our components props
+function mapStateToProps(state, ownProps) {
+  const slug = ownProps.match.params.slug;
+  const course =
+    slug && state.courses.length > 0
+      ? getCourseBySlug(state.courses, slug)
+      : newCourse;
+  return {
+    course,
+    courses: state.courses,
+    authors: state.authors,
+  };
+}
+
+//This is the bind action creators methods for dispatching the actions
+//REdux mapping function that indicates what actions we'd like to handle
+const mapDispatchToProps = {
+  loadQuotes: loadQuotes,
+};
+
+//the connect function returns a function and then that function immediately calls our component (ManageCoursePage)
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
 
 //Code saved for later to render accordion divs based for the facts, quotes, or jokes
 
