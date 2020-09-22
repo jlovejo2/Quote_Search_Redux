5a89c86572fa31c3e81c3413073b9cb4cf408ce3
James Lovejoy <james.lovejoy2@gmail.com>
2020-09-22 15:39:17 -0500

    added useEffect to load liked quotes to profile page

--- src/components/profile/ProfilePage.js
+++ src/components/profile/ProfilePage.js
@@ -6,46 +6,43 @@ import { loadLikedQuotes } from "../../redux/actions/likedQuotesActions";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
 
-const ProfilePage = () => (
-  <div className="jumbotron">
-    <h1>Profile Page</h1>
-    <p>Find all your favorited stuff here</p>
-    <Link to="/" className="btn btn-primary btn-lg">
-      Go Home
-    </Link>
-  </div>
-);
+const ProfilePage = ({ loadLikedQuotes }) => {
+  useEffect(() => {
+    loadLikedQuotes();
+  }, []);
+
+  return (
+    <div className="jumbotron">
+      <h1>Profile Page</h1>
+      <p>Find all your favorited stuff here</p>
+      <Link to="/" className="btn btn-primary btn-lg">
+        Go Home
+      </Link>
+    </div>
+  );
+};
 
 ProfilePage.propTypes = {
   likedQuotes: PropTypes.object.isRequired,
+  loadLikedQuotes: PropTypes.func.isRequired,
   history: PropTypes.object.isRequired,
 };
-
-//this type of function is commonly called a selector function
-function getCourseBySlug(courses, slug) {
-  return courses.find((course) => course.slug === slug || null);
-}
 
 //redux mapping function that indicates what states we'd like access to
 //mapStateToProps will run each time the redux store state changes
 //ownProps is automatically populated by react, it gives us access to our components props
 function mapStateToProps(state, ownProps) {
-  const slug = ownProps.match.params.slug;
-  const course =
-    slug && state.courses.length > 0
-      ? getCourseBySlug(state.courses, slug)
-      : newCourse;
+  console.log("state: ", state);
+
   return {
-    course,
-    courses: state.courses,
-    authors: state.authors,
+    likedQuotes: state.likedQuotes,
   };
 }
 
 //This is the bind action creators methods for dispatching the actions
 //REdux mapping function that indicates what actions we'd like to handle
 const mapDispatchToProps = {
-  loadQuotes: loadQuotes,
+  loadLikedQuotes,
 };
 
 //the connect function returns a function and then that function immediately calls our component (ManageCoursePage)
