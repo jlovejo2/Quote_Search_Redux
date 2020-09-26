f4a015162671c228d90cb5c3aae74f072ac76eef
James Lovejoy <james.lovejoy2@gmail.com>
2020-09-25 23:36:12 -0500

    added savecourse to likedQuotesApi

--- src/api/likedQuotesApi.js
+++ src/api/likedQuotesApi.js
@@ -27,6 +27,16 @@ export function deleteQuote(quoteId) {
     .catch(handleError);
 }
 
+export function saveCourse(quoteId) {
+  return fetch(baseUrl + (quoteId || ""), {
+    method: quoteId ? "PUT" : "POST", // POST for create, PUT to update when id already exists.
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(quoteId),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
 export function deleteQuoteClientSide(quoteId, quoteArray) {
   return quoteArray.filter((quote, index) => {
     if (index !== quoteId) {
