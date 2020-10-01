962225a9ec5746c84f156696cb582b45913f97f5
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-01 17:35:21 -0500

    added proper header to fetch for dad jokes to receive joke response

--- src/api/IcanhazdadjokeApi.js
+++ src/api/IcanhazdadjokeApi.js
@@ -3,5 +3,10 @@ const dadJokeURL = process.env.API_DAD_JOKE_URL;
 
 export function getDadJoke() {
   //promise based api that is built into modern browsers
-  return fetch(dadJokeURL).then(handleResponse).catch(handleError);
+  return fetch(dadJokeURL, {
+    method: "GET",
+    headers: { Accept: "application/json" },
+  })
+    .then(handleResponse)
+    .catch(handleError);
 }
