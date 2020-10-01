f775e4752a35c9f81aba21cce6d3fb6bfc53c0e1
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-01 16:12:17 -0500

    added code to jokePage for buttons and searching through joke apis

--- src/components/jokePage/JokePage.js
+++ src/components/jokePage/JokePage.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
@@ -9,18 +9,53 @@ import {
 import { loadAuthors } from "../../redux/actions/authorActions";
 import Spinner from "../common/Spinner";
 import { toast } from "react-toastify";
+import Jumbotron from "../common/Jumbotron";
+import { jokesApiArray } from "../api/apiInfo";
 
 const JokePage = () => {
+  const [errors, setErrors] = useState({});
+
+  const handleSearchApi = async (event) => {
+    const apiIndex = event.currentTarget.value;
+
+    try {
+      if (apiIndex === jokesApiArray[0].name) {
+        console.log("first index");
+      } else if (apiIndex === jokesApiArray[1].name) {
+        console.log("second index");
+      }
+    } catch {
+      const newError = {};
+      newError["jokesError"] = setErrors({ ...errors, newError });
+    }
+  };
+
   return (
     <Fragment>
-      <div className="jumbotron">
-        <h1>Joke Page</h1>
-        <p>Search for jokes here</p>
-        <Link to="/" className="btn btn-primary btn-lg">
-          Go Home
-        </Link>
+      <Jumbotron
+        headerOne={"Welcome to the jokes page"}
+        descriptionOne={"Select the api you would like to search for a joke"}
+      >
+        {jokesApiArray.map((quoteApi, index) => {
+          return (
+            <div key={index} className="col">
+              <button
+                key={index}
+                onClick={handleSearchApi}
+                className={"btn btn-primary"}
+                data-apiname={quoteApi.name}
+              >
+                <strong>{quoteApi.buttonHeader}</strong>
+                <hr />
+                {quoteApi.buttonText}
+              </button>
+            </div>
+          );
+        })}
+      </Jumbotron>
+      <div className="container">
+        <div className="row"></div>
       </div>
-      <div className="row"></div>
     </Fragment>
   );
 };
