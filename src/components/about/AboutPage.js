1c6f72efd903f0fe79961f5a0d7fcbb0b53ddb28
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-07 16:10:28 -0500

    changed text on about page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,8 +4,10 @@ const AboutPage = () => (
   <div>
     <h2>About</h2>
     <p>
-      This app uses React, Redux, React Router, and many other helpful
-      libraries.
+      This app uses React, Redux, React Router, and many other helpful libraries
+      to create the functionality. It uses all third-party-apis to generate the
+      quotes and jokes the those pages. Boostrap and Bootstrap-react are used as
+      the main front-end/responsiveness css styling frameworks.
     </p>
   </div>
 );
