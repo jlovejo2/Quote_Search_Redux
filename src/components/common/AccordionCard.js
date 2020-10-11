1b9e28f58b784036540733dee084a0f0c250b13f
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-11 16:08:27 -0500

    updated AccordionCard component to receive the proper props

--- src/components/common/AccordionCard.js
+++ src/components/common/AccordionCard.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { Card, Accordion } from "react-bootstrap";
-import AccordionAwareToggle from "./AccordionAwareToggle";
 
 const AccordionCard = ({
   children,
@@ -9,16 +8,13 @@ const AccordionCard = ({
   headerText,
   contextToggle,
   childrenToggle,
-  // AccordionAwareToggle,
-  // currentEventKey,
-  // toggleEventKey,
 }) => {
   return (
     <Fragment>
       <Card>
         {contextToggle ? (
           <Card.Header style={{ backgroundColor: "rgba(0,0,255,.75)" }}>
-            <AccordionAwareToggle>{childrenToggle}</AccordionAwareToggle>
+            {childrenToggle}
           </Card.Header>
         ) : (
           <Accordion.Toggle as={Card.Header} event={eventKey}>
@@ -36,10 +32,7 @@ AccordionCard.propTypes = {
   childrenToggle: PropTypes.element.isRequired,
   contextToggle: PropTypes.bool.isRequired,
   eventKey: PropTypes.string.isRequired,
-  currentEventKey: PropTypes.string.isRequired,
-  toggleEventKey: PropTypes.func.isRequired,
   headerText: PropTypes.string.isRequired,
-  AccordionAwareToggle: PropTypes.element.isRequired,
 };
 
 export default AccordionCard;
