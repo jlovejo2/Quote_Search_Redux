4544c4f8603cfa780c929a3448bdba5b3ceb0e10
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-14 16:47:30 -0500

    created map profile page

---
+++ src/components/profile/ManageProfilePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from "prop-types";
+import { ManageCoursePage } from '../courses/ManageCoursePage';
+
+export function ManageProfilePage() {
+
+    return (
+
+    )
+}
+
+ManageProfilePage.propTypes = {
+
+}
+
+function mapStateToProps() {
+
+}
+
+const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
