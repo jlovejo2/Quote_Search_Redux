497996cf0d3b079dad04736aa3dc974f81c6b5f0
James Lovejoy <james.lovejoy2@gmail.com>
2020-10-14 18:24:22 -0500

    added props to managePageProfile

--- src/components/profile/ManageProfilePage.js
+++ src/components/profile/ManageProfilePage.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
-import { ManageCoursePage } from '../courses/ManageCoursePage';
+import Spinner from '../common/Spinner';
+import CourseForm from '../courses/CourseForm';
 
-export function ManageProfilePage() {
 
-    return (
+export function ManageProfilePage({
+    authors,
+    newJokeOrQuote,
+}) {
 
-    )
+    return authors.length === 0 || newJokeOrQuote.length === 0 ? (
+        <Spinner />
+      ) : (
+        <CourseForm
+          course={course}
+        //   errors={errors}
+          authors={authors}
+          onChange={handleChange}
+          onSave={handleSave}
+        //   saving={saving}
+        />
+      );
 }
 
 ManageProfilePage.propTypes = {
-
+    authors: PropTypes.array.isRequired,
+    newJokeOrQuote: PropTypes.object.isRequired
 }
 
 function mapStateToProps() {
-
+    return {
+        authors,
+        newJokeOrQuotes,
+    }
 }
 
 const mapDispatchToProps = {
